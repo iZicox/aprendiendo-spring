e5b1f6b0b7f213c333b6054db82b31015fd17786
Francisco <53028086+iZicox@users.noreply.github.com>
2026-05-29 21:19:19 +0200

    practica spring jpa

---
+++ fundamentos-spring-jpa-todoapp-rest/src/main/java/com/fundamentos/jpa/FundamentosSpringJpaTodoappRestApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSpringJpaTodoappRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSpringJpaTodoappRestApplication.class, args);
+	}
+
+}
---
+++ fundamentos-spring-jpa-todoapp-rest/src/test/java/com/fundamentos/jpa/FundamentosSpringJpaTodoappRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSpringJpaTodoappRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
