895c1b3ec28684f412d21d3a3e27507b687da043
Francisco <53028086+iZicox@users.noreply.github.com>
2026-06-18 12:10:04 +0200

    controladores para leer tareas

--- fundamentos-spring-jpa-todoapp-rest/src/main/java/com/fundamentos/jpa/controller/TaskController.java
+++ fundamentos-spring-jpa-todoapp-rest/src/main/java/com/fundamentos/jpa/controller/TaskController.java
@@ -2,17 +2,18 @@ package com.fundamentos.jpa.controller;
 
 import com.fundamentos.jpa.dto.TaskRequest;
 import com.fundamentos.jpa.dto.TaskResponse;
-import com.fundamentos.jpa.model.BasicTask;
-import com.fundamentos.jpa.model.User;
+import com.fundamentos.jpa.model.*;
 import com.fundamentos.jpa.repos.TaskRepository;
 import com.fundamentos.jpa.repos.TaskTagRepository;
 import com.fundamentos.jpa.repos.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -44,5 +45,46 @@ public class TaskController {
                 .build(task.getId());
 
         return ResponseEntity.created(uri).body(TaskResponse.of(task));
+    }
+
+    @PostMapping("/new/checklist")
+    public ResponseEntity<TaskResponse> newChecklistTask(@RequestBody TaskRequest taskRequest) {
+        Optional<User> owner = userRepository.findByUsername(taskRequest.username());
+
+        CheckListTask task = CheckListTask.builder()
+                .owner(owner.orElse(null))
+                .title(taskRequest.title() != null ? taskRequest.title() : "Sin titulo")
+                .build();
+
+        taskRequest.itemns().stream()
+                .map(text -> CheckListItem.builder().text(text).build())
+                .forEach(task::addItem);
+
+        task = taskRepository.save(task);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/task/{id}")
+                .build(task.getId());
+
+        return ResponseEntity.created(uri).body(TaskResponse.of(task));
+    }
+
+    @GetMapping("/")
+    public Page<TaskResponse> getAll (@PageableDefault(page = 0, size = 5, sort = "createdAt") Pageable pageable) {
+
+        Page<Task> result = taskRepository.findAllWithItemsAndTags(pageable);
+
+        if(result.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found");
+        }
+
+        return result.map(TaskResponse::of);
+    }
+
+    @GetMapping("/{id}")
+    public TaskResponse getTaskById(@PathVariable Long id) {
+        return taskRepository.findByIdWithItemsAndTags(id)
+                .map(TaskResponse::of)
+                .orElseThrow(()  -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Task with id %d not found".formatted(id)));
     }
 }
--- fundamentos-spring-jpa-todoapp-rest/src/main/java/com/fundamentos/jpa/repos/TaskRepository.java
+++ fundamentos-spring-jpa-todoapp-rest/src/main/java/com/fundamentos/jpa/repos/TaskRepository.java
@@ -1,7 +1,22 @@
 package com.fundamentos.jpa.repos;
 
 import com.fundamentos.jpa.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @Query("""
+        select t from Task t
+        left join fetch t.items
+        left join fetch t.tags
+    """)
+    Page<Task> findAllWithItemsAndTags(Pageable pageable);
+
+    Optional<Task> findByIdWithItemsAndTags(Long id);
+
 }
