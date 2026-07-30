ed034910a18832502799656549d8d23b93ba27a6
Francisco <53028086+iZicox@users.noreply.github.com>
2026-07-30 13:35:08 +0200

    actualizar

---
+++ fundamentos-practica/practica-2/src/main/java/com/springboot/practica_2/dto/ProductoDTO.java
@@ -0,0 +1,31 @@
+package com.springboot.practica_2.dto;
+
+import com.springboot.practica_2.model.Producto;
+
+public record ProductoDTO(
+        Long id, String nombre, double precio
+) {
+
+    // entidad a dto
+    public static ProductoDTO crearDTO(Producto producto) {
+        return new ProductoDTO(
+                producto.getId(),
+                producto.getNombre(),
+                producto.getPrecio()
+        );
+    }
+
+    // dto a producto
+    public Producto crearProducto() {
+        return Producto.builder()
+                .nombre(this.nombre)
+                .precio(this.precio)
+                .build() ;
+    }
+
+    // editar
+    public void editar(Producto producto) {
+        producto.setNombre(this.nombre);
+        producto.setPrecio(this.precio);
+    }
+}
---
+++ fundamentos-practica/practica-2/src/main/java/com/springboot/practica_2/model/Producto.java
@@ -0,0 +1,46 @@
+package com.springboot.practica_2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Producto {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nombre;
+    private double precio;
+
+    @Builder
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Producto producto = (Producto) o;
+        return getId() != null && Objects.equals(getId(), producto.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
